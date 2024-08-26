7fe1f974e003c81c8699c3c91a672cc83416ded3
Le-Kiet <116272203+Le-Kiet@users.noreply.github.com>
2024-08-26 14:56:56 +0700

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NotFound from './pages/NotFound';
-import Home from './pages/Home';
-import OnGoing from './pages/OnGoing';
-import UpComing from './pages/UpComing';
-import ShowTime from './pages/ShowTime'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import OnGoing from "./pages/OnGoing";
+import UpComing from "./pages/UpComing";
+import ShowTime from "./pages/ShowTime";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/movie/showing/" element={<OnGoing />} />
         <Route path="/movie/upcoming/" element={<UpComing />} />
         <Route path="/showtimes/" element={<ShowTime />} />
+        <Route path="/login/" element={<Login />} />
+        <Route path="/forgotpassword/" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
